f709a61b99929b08adb2398dd1ab1bd743b10d9d
duncangraham <duncangraham75@gmail.com>
2014-03-15 15:46:11 -0400

    logo shiznit

--- antimatter.js
+++ antimatter.js
@@ -1,15 +1,12 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to antimatter.";
-  };
+  // Template.hello.greeting = function () {
+  //   return "antimatter.";
+  // };
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  // Template.hello.events({
+  //   'click input': function () {
+  //   }
+  // });
 }
 
 if (Meteor.isServer) {
