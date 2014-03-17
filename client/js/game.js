e1219c4ddab5881408e16008b8b96a36ba718090
Thomas Morris <thomas.imorris@gmail.com>
2014-03-17 22:01:07 +0100

    Add description comments to functions

--- client/js/game.js
+++ client/js/game.js
@@ -10,6 +10,12 @@ $(function(){ //DOM Ready
 	  
   }
 
+
+  //
+  // Template.game.users
+  // ===================
+  // returns the current number of users that have cast their vote
+  //
   Template.game.users = function() {
     //get number of users
     numUsers = Votes.findOne({voteType: "up"}).amount 
@@ -19,6 +25,12 @@ $(function(){ //DOM Ready
   }
 
 
+  //
+  // Template.game.upVotes
+  // ===================
+  // returns the current number of votes received for "up" (i.e. - how
+  // many people think that anti-matter will fall upward)
+  //
   Template.game.upVotes = function() {
     //get number of upvotes
     // return Votes.findOne({voteType: "up"}).amount;
@@ -28,6 +40,18 @@ $(function(){ //DOM Ready
   }
 
 
+  //
+  // Template.game.voteCount
+  // ===================
+  // Returns a positive or negative number, depending on whether or not
+  // the majority of the audience voted for antimatter falling up or down.
+  //
+  // Example: If 10 people vote it will fall "up" and 17 people vote that
+  // it will fall "down", this function will return -7
+  //
+  // Example 2: if 10 people vote "up" and 4 people vote "down", this
+  // function will return 6
+  // 
   Template.game.voteCount = function() {
     numUpVotes = Votes.findOne({voteType: "up"}).amount 
     numDownVotes = Votes.findOne({voteType: "down"}).amount 
