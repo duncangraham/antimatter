0822b8bfb96cd407e538994f9f35b9e37dea25f0
Thomas Morris <thomas.imorris@gmail.com>
2014-03-17 21:12:04 +0100

    Make skeletons for template functions

--- client/js/game.js
+++ client/js/game.js
@@ -1,3 +1,15 @@
 Template.game.rendered = function() {
 	
 }
+
+Template.game.users = function() {
+  //get number of users
+}
+
+Template.game.upVotes = function() {
+  //get number of upvotes
+}
+
+Template.game.voteCount = function() {
+  //get total number of all votes submitted
+}
