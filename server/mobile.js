a972efe161cf35f563aa202379272c823425f0d5
Thomas Morris <thomas.imorris@gmail.com>
2014-03-17 14:00:13 +0100

    Populate database if it's empty

---
+++ server/mobile.js
@@ -0,0 +1,16 @@
+// database has the schema:
+//   voteType:     amount:
+//   "up"          3
+//   "down"        7
+
+Meteor.startup(function () {
+  Votes = new Meteor.Collection("votes");
+
+  if (Votes.find().count() === 0) {
+    var voteTypes = ["up", "down"];
+
+    for (var i = 0; i < voteTypes.length; i++)
+      Votes.insert({voteType: voteTypes[i], amount: 0});
+  }
+});
+
