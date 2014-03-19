e4c910378f205f502e3eb81a686b4aae865994e1
Thomas Morris <thomas.imorris@gmail.com>
2014-03-19 11:38:58 +0100

    Make play/pause button

--- client/js/implications.js
+++ client/js/implications.js
@@ -1,5 +1,17 @@
-Template.implications.events = function() {
+var videoPaused = false;
 
+Template.implications.events = {
+  //Toggle play/pause with img.play-button
+  "click  img.play-button": function() {
+    if (videoPaused){
+      implicationsVid.play();
+      videoPaused = false;
+    }
+    else {
+      implicationsVid.pause()
+      videoPaused = true;
+    }
+  }
 }
 
 Template.implications.rendered = function() {
