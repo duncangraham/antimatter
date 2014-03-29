774d600c1f88364f858b2503389956151a5ad7f3
duncangraham <duncangraham75@gmail.com>
2014-03-28 23:52:46 -0400

    little responsive fixes

--- client/js/drawSVG.js
+++ client/js/drawSVG.js
@@ -1,3 +1,7 @@
 Template.landing.rendered = function() {
-	$('.bgvid')[0].play()
+	$('.bgvid')[0].play();
+
+	var bottomCenter = $('.bottom-center');
+	var margin = bottomCenter.width()/2;
+	bottomCenter.css('margin-left', -margin);
 }
