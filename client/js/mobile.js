1b05d864149487c29c68ba96f5c831d1177a520e
Thomas Morris <thomas.imorris@gmail.com>
2014-03-30 21:41:49 +0300

    Make debugging mode in mobile

--- client/js/mobile.js
+++ client/js/mobile.js
@@ -1,39 +1,40 @@
+//make "Constants"
+var UP = 1,
+    DOWN = -1,
+    NOT_SET = 0;
+
+//Database collections
+//    database has the schema:
+//      voteType:     amount:
+//      "up"          3
+//      "down"        7
+
+// while( Votes.findOne() === undefined ){
+//   //wait till the db is ready
+// }
+Session.set("myVote", NOT_SET);
+// Session.set( "upId", Votes.findOne({voteType: 'up'})._id );
+// Session.set( "downId", Votes.findOne({voteType: 'down'})._id );
+
 Template.mobile.rendered = function() {
-
-
-  //make "Constants"
-  var UP = 1,
-      DOWN = -1,
-      NOT_SET = 0;
 
 
   // var beginButtonPushed = false
 
-  Session.set("myVote", NOT_SET);
-  Session.set( "upId", Votes.findOne({voteType: 'up'})._id );
-  Session.set( "downId", Votes.findOne({voteType: 'down'})._id );
-
-
-  //Database collections
-  //    database has the schema:
-  //      voteType:     amount:
-  //      "up"          3
-  //      "down"        7
-
-
   //Capture device orientation
-  if (window.DeviceOrientationEvent) {
-
-    // Listen for the event and handle DeviceOrientationEvent object
-    window.addEventListener('deviceorientation', 
-                            handleTilting, 
-                            false);
-  }
-  else {
-    $(".orientation").html("Sorry, your device doesn't have tilt " +
-      "recognition");
-  }
-
+  // TODO: uncomment this after debugging
+  // if (window.DeviceOrientationEvent) {
+  //
+  //   // Listen for the event and handle DeviceOrientationEvent object
+  //   window.addEventListener('deviceorientation', 
+  //                           // handleTilting, 
+  //                           tiltDebugging,
+  //                           false);
+  // }
+  // else {
+  //   $(".orientation").html("Sorry, your device doesn't have tilt " +
+  //     "recognition");
+  // }
 
   //for now, make the voting binary.  Only up or down
   function handleTilting(eventData){
@@ -56,85 +57,122 @@ Template.mobile.rendered = function() {
         voteDown();// submit a vote of "down"
       }
   }
+}
 
+//Template functions
+//TODO: might consider wrapping Template.mobile.votes in
+//a Template.post_edit.helpers({}) block
+Template.mobile.votes = function () {
+  return Votes.find();
+};
 
-  function voteUp(){
-    // If there was no previous vote, make "up"+1
-    if (Session.get("myVote") == NOT_SET) {
-      console.log("NOT_SET")
-      dbChangeVotes(Session.get("upId"), 1);
-      Session.set("myVote", UP);
-      // outputDebugging();
-      return
-    }
+Template.mobile.events = {
+  'click #vote-up': function(e) {
+    voteUp();
+    outputDebugging();
+  },
 
-    // If previous vote was "down", make "up"+1 and "down"-1
-    if (Session.get("myVote") == DOWN) {
-      console.log("DOWN")
-      Session.set("myVote", UP);
-      dbChangeVotes(Session.get("upId"), 1);
-      dbChangeVotes(Session.get("downId"), -1);
-      // outputDebugging();
-    }
-  }
-
-
-  function voteDown(){
-    // If there was no previous vote, make "down"+1
-    if (Session.get("myVote") == NOT_SET) {
-      console.log("NOT_SET")
-      dbChangeVotes(Session.get("downId"), 1);
-      Session.set("myVote", DOWN);
-      // outputDebugging();
-      return
-    }
-
-    // If previous vote was "up", make "down"+1 and "up"-1
-    if (Session.get("myVote") == UP) {
-      console.log("UP")
-      Session.set("myVote", DOWN);
-      dbChangeVotes(Session.get("downId"), 1);
-      dbChangeVotes(Session.get("upId"), -1);
-      // outputDebugging();
-    }
-  }
-
-
-  function dbChangeVotes( direction, changeBy ){
-    console.log(Votes.find().fetch());
-    Votes.update(
-      {_id: direction}, 
-      {$inc: {amount: changeBy}});
-    }
-
-
-  //Template functions
-  Template.mobile.votes = function () {
-    return Votes.find();
-  };
-
-
-
-  // We aren't going to try to wipe the user's vote when they close the
-  // browser anymore.  We're going to make the db wipe
-  // when the "game" page is loaded.  We're going to make it unable to
-  // access the page without a username and password
-  //
-  // // remove this user's vote from the DB when they close the mobile page
-  // //SCARY WARNING!!! THIS IS UNTESTED AS OF YET.  PROCEED WITH CAUTION!
-  // window.onbeforeunload = function() {
-  //   if (Session.get("myVote") == DOWN) {
-  //     dbChangeVotes( "down", -1 );
-  //   }
-  //   else if (Session.get("myVote") == UP) {
-  //     dbChangeVotes( "up", -1 );
-  //   }
-  // }
-  //
-
-  function outputDebugging() {
-    $(".orientation").html(
-        "Your vote = " + Session.get("myVote") + " " + Session.get('upId') + " " + Session.get('downId'));
+  'click #vote-down': function(e) {
+    voteDown();
+    outputDebugging();
   }
 }
+
+function voteUp(){
+  var upID = Votes.findOne({voteType: 'up'})._id,
+      downID = Votes.findOne({voteType: 'down'})._id
+
+  //debugging output
+  var upVotes = Votes.findOne({voteType: "up"}).amount
+  var downVotes = Votes.findOne({voteType: "down"}).amount
+  console.log("total votes before yours was cast: Upvotes = " + upVotes + ", Downvotes = " + downVotes);
+
+  // If there was no previous vote, make "up"+1
+  if (Session.get("myVote") == NOT_SET) {
+    console.log("previous vote was NOT_SET")
+    dbChangeVotes(upID, 1);
+    Session.set("myVote", UP);
+    // outputDebugging();
+    // return
+  }
+
+  // If previous vote was "down", make "up"+1 and "down"-1
+  if (Session.get("myVote") == DOWN) {
+    console.log("previous vote was DOWN")
+    Session.set("myVote", UP);
+    dbChangeVotes(upID, 1);
+    dbChangeVotes(Session.get("downID"), -1);
+    // outputDebugging();
+  }
+
+  //debugging output
+  upVotes = Votes.findOne({voteType: "up"}).amount
+  downVotes = Votes.findOne({voteType: "down"}).amount
+  console.log("total votes after yours was cast: Upvotes = " + upVotes + ", Downvotes = " + downVotes);
+}
+
+
+function voteDown(){
+  var upID = Votes.findOne({voteType: 'up'})._id,
+      downID = Votes.findOne({voteType: 'down'})._id
+
+  //debugging output
+  var upVotes = Votes.findOne({voteType: "up"}).amount
+  var downVotes = Votes.findOne({voteType: "down"}).amount
+  console.log("total votes before yours was cast: Upvotes = " + upVotes + ", Downvotes = " + downVotes);
+
+  // If there was no previous vote, make "down"+1
+  if (Session.get("myVote") == NOT_SET) {
+    console.log("previous vote was NOT_SET")
+    dbChangeVotes(downID, 1);
+    Session.set("myVote", DOWN);
+    // outputDebugging();
+    // return
+  }
+
+  // If previous vote was "up", make "down"+1 and "up"-1
+  if (Session.get("myVote") == UP) {
+    console.log("previous vote was UP")
+    Session.set("myVote", DOWN);
+    dbChangeVotes(downID, 1);
+    dbChangeVotes(upID, -1);
+    // outputDebugging();
+  }
+
+  //debugging output
+  upVotes = Votes.findOne({voteType: "up"}).amount
+  downVotes = Votes.findOne({voteType: "down"}).amount
+  console.log("total votes after yours was cast: Upvotes = " + upVotes + ", Downvotes = " + downVotes);
+}
+
+
+function dbChangeVotes( direction, changeBy ){
+  Votes.update(
+    {_id: direction}, 
+    {$inc: {amount: changeBy}});
+}
+
+
+//Debugging functions
+function outputDebugging() {
+  console.log("Your vote = " + Session.get("myVote"));
+}
+
+
+// We aren't going to try to wipe the user's vote when they close the
+// browser anymore.  We're going to make the db wipe
+// when the "game" page is loaded.  We're going to make it unable to
+// access the page without a username and password
+//
+// // remove this user's vote from the DB when they close the mobile page
+// //SCARY WARNING!!! THIS IS UNTESTED AS OF YET.  PROCEED WITH CAUTION!
+// window.onbeforeunload = function() {
+//   if (Session.get("myVote") == DOWN) {
+//     dbChangeVotes( "down", -1 );
+//   }
+//   else if (Session.get("myVote") == UP) {
+//     dbChangeVotes( "up", -1 );
+//   }
+// }
+//
 
